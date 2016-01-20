f3c418f78ffc8113ed73c06ab6c6ccffc763d10c
Namida Aneskans <namida@skunkfrukt.se>
2016-01-20 11:18:34 +0100

    Added support for supplying pattern as GET param.

--- ui.js
+++ ui.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
 		var example = window.ReverseRegexThing.parse($("#regex").val());
 		$("<div class=\"regexExample\" />").text(example).prependTo($("#exampleOutput"));
 	});
+	
+	if (window.location.search) {
+		var getItems = window.location.search.substr(1).split('&');
+		var getParameters = {};
+		for (var i = 0; i < getItems.length; i++) {
+			var keyVal = getItems[i].split('=');
+			getParameters[keyval[0]] = keyVal[1];
+		}
+		
+		if (getParameters["pattern"]) {
+			$("#regex").val(decodeURIComponent(getParameters["pattern"]));
+		}
+	}
 });
